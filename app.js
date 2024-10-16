5551fb64a1c5fb391514b6ec0d248fca9e411e93
missfionajean <missfionajean@gmail.com>
2024-10-16 12:30:12 -0400

    added looping & capitalization to username input, as well as default responses for non-answers in username & ready prompts

--- app.js
+++ app.js
@@ -2,12 +2,28 @@
 //set up prompt variable to be used throughout
 const prompt = require('prompt-sync')();
 
-//get player's name and welcome them
-const username = prompt('What is your name? ');
-console.log(`Welcome, ${username}!`);
+//function to establish player's name
+function getUsername() {
+    let nonAnswers = 0;
+    while (nonAnswers < 3) {
+        const name = prompt('What is your name? ');
+        if (name.length == 0) {
+            console.log('I have to call you SOMETHING. Let\'s try that again.');
+            nonAnswers++;
+        } else {
+            return name.charAt(0).toUpperCase() + name.slice(1);
+        }
+    }
+    console.log('Whatever, I\'m calling you Kevin.');
+    return 'Kevin';
+}
 
-//comical ready prompt (infinite loop)
-while (true) {
+const username = getUsername();
+console.log(`Welcome to GAME NAME, ${username}!`);
+
+//comical ready prompt loop
+let nonAnswers = 0;
+while (nonAnswers < 3) {
     const imReady = prompt('Are you ready? \(Y/N\) ');
     if (imReady == 'y') {
         console.log(`That\'s the spirit, ${username}! Let's go!`);
@@ -17,5 +33,10 @@ while (true) {
         break;
     } else {
         console.log('You have to answer Y or N. I don\'t make the rules, I just enforce them.');
+        nonAnswers++;
     }
 };
+
+if (nonAnswers == 3) {
+    console.log(`You don't follow rules very well, ${username}. Here we go anyway!`);
+}
